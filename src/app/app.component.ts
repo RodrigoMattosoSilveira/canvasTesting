b8552b8a74e3d82baf091004f3568b20d2057a00
RodrigoMattosoSilveira <r3850365@gmail.com>
2019-06-21 07:57:42 -0700

    master  -  Added a canvas and drew a Hello World message with a ci image on it

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Input, ViewChild  } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+   selector: 'app-root',
+   templateUrl: './app.component.html',
+   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'canvasTesting';
+   @ViewChild('myCanvas') myCanvas: ElementRef;
+
+   title = 'canvasTesting';
+   canvasEl: HTMLCanvasElement
+   ctx: CanvasRenderingContext2D;
+
+   // see https://blog.angular-university.io/angular-viewchild/
+   // for recommendation to use ngAfterViewInit instead of ngOnInit
+   ngAfterViewInit() {
+      this.canvasEl = this.myCanvas.nativeElement
+      this.ctx = this.canvasEl.getContext('2d');
+      this.ctx.font = "30px Arial";
+      this.ctx.strokeText("Hello World", 10, 50);
+      this.ctx.beginPath();
+      this.ctx.arc(95, 50, 40, 0, 2 * Math.PI);
+      this.ctx.stroke();
+   }
 }
